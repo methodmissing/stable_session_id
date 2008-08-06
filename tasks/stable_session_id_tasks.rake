8566b60e349bcd89ab0538c207b48beeea355ff1
Lourens Naude <lourens@methodmissing.com>
2008-08-06 19:01:56 +0100

    plugin structure

---
+++ tasks/stable_session_id_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :stable_session_id do
+#   # Task goes here
+# end
---
+++ test/stable_session_id_test.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+
+class StableSessionIdTest < Test::Unit::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
