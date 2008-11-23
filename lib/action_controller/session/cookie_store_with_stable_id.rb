13a96d1306e689b1025e4fc9fecb9bd27a13df50
Old Macbook Pro <lourens@methodmissing.com>
2008-11-23 22:47:29 +0000

    temp. logging

--- lib/action_controller/session/cookie_store_with_stable_id.rb
+++ lib/action_controller/session/cookie_store_with_stable_id.rb
@@ -15,24 +15,29 @@ module ActionController
       module InstanceMethods
         
         def initialize_with_stable_id( session, options = {} )
+          log("initialize with stable id")
           options.reverse_merge!( 'stable_session_id' => false )
           @stable_session_id = options['stable_session_id']
           initialize_without_stable_id( session, options )
         end
         
         def marshal_with_stable_id( session )
+          log("marshal with stable id")
           session = stable_session_id!( session )
           marshal_without_stable_id( session )
         end
         
         def unmarshal(cookie)
+          log("unmarshal cookie")
           if cookie
+            log("have a cookie")
             cookie_data = verifier.verify(cookie)
             returning( stable_session_id!( cookie_data ) ) do |data|
                @session.instance_variable_set(:@session_id, data[:session_id]) if @stable_session_id 
             end
           end
           rescue ActiveSupport::MessageVerifier::InvalidSignature
+            log("invalid cookie signature")
             delete
             raise TamperedWithCookie
         end        
@@ -44,11 +49,17 @@ module ActionController
 
         def inject_stable_session_id!( data )
           if data.respond_to?(:key?) && !data.key?( :session_id )
+            log("inject stable session id")
             { :session_id => CGI::Session.generate_unique_id }
           else
+            log("do not inject stable session id")
            {}
           end  
         end        
+        
+        def log( message )
+          ::ActionController::Base.logger.info( "** #{message}" ) if ::ActionController::Base.logger
+        end
         
       end
             
