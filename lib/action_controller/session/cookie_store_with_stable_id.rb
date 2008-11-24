733e0cd1070b0176c87c6f46204ee43e85c1ac26
Old Macbook Pro <lourens@methodmissing.com>
2008-11-24 10:30:38 +0000

    remove logging

--- lib/action_controller/session/cookie_store_with_stable_id.rb
+++ lib/action_controller/session/cookie_store_with_stable_id.rb
@@ -15,68 +15,40 @@ module ActionController
       module InstanceMethods
         
         def initialize_with_stable_id( session, options = {} )
-          log("initialize with stable id")
           options.reverse_merge!( 'stable_session_id' => false )
           @stable_session_id = options['stable_session_id']
-          log("initialize without stable id")
           initialize_without_stable_id( session, options )
         end
         
         def marshal_with_stable_id( session )
-          log("marshal with stable id")
           session = stable_session_id!( session )
-          log( session.inspect )
-          log_session_id
           marshal_without_stable_id( session )
         end
         
         def unmarshal(cookie)
-          log("unmarshal cookie")
           if cookie
-            begin
-              log("have a cookie")
-              log_session_id
-              cookie_data = verifier.verify(cookie)
-              log(cookie_data)
-              stable_session_id!( cookie_data )
-              log( session_id )
-            rescue ActiveSupport::MessageVerifier::InvalidSignature
-              log("invalid cookie signature")
-              log_session_id
-              delete
-              raise TamperedWithCookie
-            end
+            cookie_data = verifier.verify(cookie)
+            stable_session_id!( cookie_data )
           end
+          rescue ActiveSupport::MessageVerifier::InvalidSignature
+            delete
+            raise TamperedWithCookie
         end        
         
         def stable_session_id!( data  )
-          log( data.inspect )
           return data unless @stable_session_id
-          log("stable_session_id!")
           ( data ||= {} ).merge!( inject_stable_session_id!( data ) )
           @session.instance_variable_set(:@session_id, data[:session_id])
-          log( @session.inspect )
           data
         end
 
         def inject_stable_session_id!( data )
-          log( data.inspect )
           if data.respond_to?(:key?) && !data.key?( :session_id )
-            log("inject stable session id")
             { :session_id => CGI::Session.generate_unique_id }
           else
-            log("do not inject stable session id")
            {}
           end  
         end        
-        
-        def log_session_id
-          log @session.instance_variable_get(:@session_id)
-        end
-        
-        def log( message )
-          ::ActionController::Base.logger.info( "** #{message}" ) if ::ActionController::Base.logger
-        end
         
       end
             
--- lib/action_controller/session_cgi_ext/session_id.rb
+++ lib/action_controller/session_cgi_ext/session_id.rb
@@ -2,7 +2,6 @@ class CGI
   class Session
   
     def session_id
-      ::ActionController::Base.logger.info "** custom session_id"
       @data ||= @dbman.restore
       if @data[:session_id]
         @data[:session_id]
